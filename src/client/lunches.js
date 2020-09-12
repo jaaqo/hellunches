1f7fe29a2c9055652ca735933da3e752a0b1b31a
Jaakko Salokanta <jaakko.salokanta@gmail.com>
2020-09-12 12:41:45 +0300

    Clean Next.js

--- src/client/lunches.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-const base = process.env.REACT_APP_API_URL_BASE
-
-export const getLunches = async () => {
-  const {data: lunches} = await axios.get(`${base}/lunches`)
-  return lunches
-}
