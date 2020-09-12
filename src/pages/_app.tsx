86acbd8dbe44b13c43eb01ebf1ff5135b5fd85f7
Jaakko Salokanta <jaakko.salokanta@gmail.com>
2020-09-12 14:23:15 +0300

    Add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,31 @@
+import Head from 'next/head'
+
 import 'skeleton-css/css/normalize.css'
 import 'skeleton-css/css/skeleton.css'
 import '../index.css'
 
-// This default export is required in a new `pages/_app.js` file.
 export default function MyApp({Component, pageProps}) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
